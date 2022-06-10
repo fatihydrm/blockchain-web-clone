9888488b70ef3074629a9b62f9e0873b923c5198
fatihydrm <fatihydrm@outlook.com>
2022-06-10 11:49:23 +0300

    update

--- components/js/app.js
+++ components/js/app.js
@@ -1,9 +1,10 @@
 const bigCard1 = document.querySelectorAll("#bigCard1 ul li");
 const bigCard1Img = document.querySelectorAll("#bigCard1 .right img");
 
-console.log(bigCard1Img);
+const bigCard2 = document.querySelectorAll("#bigCard2 ul li");
+const bigCard2Img = document.querySelectorAll("#bigCard2 .right img");
 
-function clearClassList() {
+function clearClassList1() {
   for (let i = 0; i < bigCard1.length; i++) {
     bigCard1[i].classList.remove("active");
     bigCard1Img[i].classList.remove("active");
@@ -12,9 +13,26 @@ function clearClassList() {
 
 bigCard1.forEach((e, i) => {
   e.addEventListener("click", () => {
-    clearClassList();
+    clearClassList1();
     e.classList.add("active");
     console.log(bigCard1Img[i]);
     bigCard1Img[i].classList.add("active");
+  });
+});
+
+
+function clearClassList2() {
+  for (let i = 0; i < bigCard2.length; i++) {
+    bigCard2[i].classList.remove("active");
+    bigCard2Img[i].classList.remove("active");
+  }
+}
+
+bigCard2.forEach((e, i) => {
+  e.addEventListener("click", () => {
+    clearClassList2();
+    e.classList.add("active");
+    console.log(bigCard2Img[i]);
+    bigCard2Img[i].classList.add("active");
   });
 });
