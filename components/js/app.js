94d51fd52229706e48b233ac738ee0ed2a6e8b0b
Fatih Şen <fatihydrm@outlook.com>
2022-06-12 20:19:29 +0300

    update

--- components/js/app.js
+++ components/js/app.js
@@ -38,3 +38,253 @@ bigCard4.forEach((e, i) => {
     bigCard4Img[i].classList.add("active");
   });
 });
+
+// Img inner
+let img1 = document.querySelector("#img1");
+let img2 = document.querySelector("#img2");
+let img4 = document.querySelector("#img4");
+
+// Text inner
+let textInner1 = document.querySelector("#textInner1");
+let textInner2 = document.querySelector("#textInner2");
+let textInner4 = document.querySelector("#textInner4");
+
+// Back Next Button
+let imgBackBtn1 = document.querySelector("#imgBackBtn1");
+let imgNextBtn1 = document.querySelector("#imgNextBtn1");
+let imgBackBtn2 = document.querySelector("#imgBackBtn2");
+let imgNextBtn2 = document.querySelector("#imgNextBtn2");
+let imgBackBtn4 = document.querySelector("#imgBackBtn4");
+let imgNextBtn4 = document.querySelector("#imgNextBtn4");
+
+// Slider wave
+let wave1 = document.querySelectorAll("#wave1 span");
+let wave2 = document.querySelectorAll("#wave2 span");
+let wave4 = document.querySelectorAll("#wave4 span");
+
+let count1 = 0;
+let count2 = 0;
+
+wave1.forEach((e, i) => {
+  e.addEventListener("click", () => {
+    for (let i = 0; i < wave1.length; i++) {
+      wave1[i].classList.remove("active");
+    }
+    e.classList.add("active");
+
+    textInner1.style.transform = `translateX(-${i}00%)`;
+    img1.style.transform = `translateX(-${i}00%)`;
+    count1 = i;
+  });
+});
+
+wave2.forEach((e, i) => {
+  e.addEventListener("click", () => {
+    for (let i = 0; i < wave2.length; i++) {
+      wave2[i].classList.remove("active");
+    }
+    e.classList.add("active");
+
+    textInner2.style.transform = `translateX(-${i}00%)`;
+    img2.style.transform = `translateX(-${i}00%)`;
+    count2 = i;
+  });
+});
+
+wave4.forEach((e, i) => {
+  e.addEventListener("click", () => {
+    for (let i = 0; i < wave4.length; i++) {
+      wave4[i].classList.remove("active");
+    }
+    e.classList.add("active");
+
+    textInner4.style.transform = `translateX(-${i}00%)`;
+    img4.style.transform = `translateX(-${i}00%)`;
+    count2 = i;
+  });
+});
+
+imgBackBtn1.addEventListener("click", (e) => {
+  for (let i = 0; i < wave1.length; i++) {
+    wave1[i].classList.remove("active");
+  }
+  switch (count1) {
+    case 0:
+      img1.style.transform = `translateX(-400%)`;
+      wave1[4].classList.add("active");
+      count1 = 4;
+      break;
+    case 1:
+      img1.style.transform = `translateX(0%)`;
+      wave1[0].classList.add("active");
+      count1--;
+      break;
+    case 2:
+      img1.style.transform = `translateX(-100%)`;
+      wave1[1].classList.add("active");
+      count1--;
+      break;
+    case 3:
+      img1.style.transform = `translateX(-200%)`;
+      wave1[2].classList.add("active");
+      count1--;
+      break;
+    case 4:
+      img1.style.transform = `translateX(-300%)`;
+      wave1[3].classList.add("active");
+      count1--;
+      break;
+  }
+});
+imgNextBtn1.addEventListener("click", (e) => {
+  for (let i = 0; i < wave1.length; i++) {
+    wave1[i].classList.remove("active");
+  }
+  switch (count1) {
+    case 0:
+      img1.style.transform = `translateX(-100%)`;
+      wave1[1].classList.add("active");
+      count1++;
+      break;
+    case 1:
+      img1.style.transform = `translateX(-200%)`;
+      wave1[2].classList.add("active");
+      count1++;
+      break;
+    case 2:
+      img1.style.transform = `translateX(-300%)`;
+      wave1[3].classList.add("active");
+      count1++;
+      break;
+    case 3:
+      img1.style.transform = `translateX(-400%)`;
+      wave1[4].classList.add("active");
+      count1++;
+      break;
+    case 4:
+      img1.style.transform = `translateX(0%)`;
+      wave1[0].classList.add("active");
+      count1 = 0;
+      break;
+  }
+});
+
+imgBackBtn2.addEventListener("click", (e) => {
+  for (let i = 0; i < wave2.length; i++) {
+    wave2[i].classList.remove("active");
+  }
+  switch (count2) {
+    case 0:
+      img2.style.transform = `translateX(-300%)`;
+      wave2[3].classList.add("active");
+      count2 = 3;
+      break;
+    case 1:
+      img2.style.transform = `translateX(0%)`;
+      wave2[0].classList.add("active");
+      count2--;
+      break;
+    case 2:
+      img2.style.transform = `translateX(-100%)`;
+      wave2[1].classList.add("active");
+      count2--;
+      break;
+    case 3:
+      img2.style.transform = `translateX(-200%)`;
+      wave2[2].classList.add("active");
+      count2--;
+      break;
+    case 4:
+      img2.style.transform = `translateX(-300%)`;
+      wave2[3].classList.add("active");
+      count2--;
+      break;
+  }
+});
+imgNextBtn2.addEventListener("click", (e) => {
+  for (let i = 0; i < wave2.length; i++) {
+    wave2[i].classList.remove("active");
+  }
+  switch (count2) {
+    case 0:
+      img2.style.transform = `translateX(-100%)`;
+      wave2[1].classList.add("active");
+      count2++;
+      break;
+    case 1:
+      img2.style.transform = `translateX(-200%)`;
+      wave2[2].classList.add("active");
+      count2++;
+      break;
+    case 2:
+      img2.style.transform = `translateX(-300%)`;
+      wave2[3].classList.add("active");
+      count2++;
+      break;
+    case 3:
+      img2.style.transform = `translateX(-0%)`;
+      wave2[0].classList.add("active");
+      count2 = 0;
+      break;
+  }
+});
+
+imgBackBtn4.addEventListener("click", (e) => {
+  for (let i = 0; i < wave4.length; i++) {
+    wave4[i].classList.remove("active");
+  }
+  switch (count2) {
+    case 0:
+      img4.style.transform = `translateX(-300%)`;
+      wave4[3].classList.add("active");
+      count2 = 3;
+      break;
+    case 1:
+      img4.style.transform = `translateX(0%)`;
+      wave4[0].classList.add("active");
+      count2--;
+      break;
+    case 2:
+      img4.style.transform = `translateX(-100%)`;
+      wave4[1].classList.add("active");
+      count2--;
+      break;
+    case 3:
+      img4.style.transform = `translateX(-200%)`;
+      wave4[2].classList.add("active");
+      count2--;
+      break;
+    case 4:
+      img4.style.transform = `translateX(-300%)`;
+      wave4[3].classList.add("active");
+      count2--;
+      break;
+  }
+});
+imgNextBtn4.addEventListener("click", (e) => {
+  for (let i = 0; i < wave4.length; i++) {
+    wave4[i].classList.remove("active");
+  }
+  switch (count2) {
+    case 0:
+      img4.style.transform = `translateX(-100%)`;
+      wave4[1].classList.add("active");
+      count2++;
+      break;
+    case 1:
+      img4.style.transform = `translateX(-200%)`;
+      wave4[2].classList.add("active");
+      count2++;
+      break;
+    case 2:
+      img4.style.transform = `translateX(-300%)`;
+      wave4[3].classList.add("active");
+      count2++;
+      break;
+    case 3:
+      img4.style.transform = `translateX(-0%)`;
+      wave4[0].classList.add("active");
+      count2 = 0;
+      break;
+  }
+});
